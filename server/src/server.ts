0575cd54b2e876ae3e633341814187e1f10d3f9e
Subodh-Dhamala <subodhdhamala@gmail.com>
2026-06-23 07:17:15 +0545

    initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,36 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import { AppDataSource } from "./lib/database";
+
+const app = express();
+const PORT = process.env.PORT || 4500;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is working fine!");
+});
+
+async function startServer() {
+  try {
+    await AppDataSource.initialize();
+    console.log("DB connected!");
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening at http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("DB connection failed", err);
+    process.exit(1);
+  }
+}
+
+startServer();
+
+export default app;
